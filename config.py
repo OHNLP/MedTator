eb670f552f8755a65d44860d8f75ef323d438347
Huan He <hehuan2112@gmail.com>
2021-11-11 22:07:47 -0600

    convert lib url to configs

--- config.py
+++ config.py
@@ -41,8 +41,43 @@ THIRD_PARTY_LIB_URL={
         'CSS': "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css",
         'JS': "https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"
     },
+    'VUE': {
+        'JS': "https://cdn.jsdelivr.net/npm/vue@2.6.11"
+    },
+    'JSZIP': {
+        'JS': 'https://stuk.github.io/jszip/dist/jszip.js'
+    },
+    'FILESAVER': {
+        'JS': "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js"
+    },
+    'DAYJS': {
+        'JS': "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.8.36/dayjs.min.js"
+    },
     'CODE_MIRROR': {
         'CSS': "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/codemirror.min.css",
-        'JS': ""
+        'JS': "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0/codemirror.min.js",
+        'JS_ADDON_ACTIVE_LINE': "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.2/addon/selection/active-line.min.js"
+    },
+    'NUMJS': {
+        'JS': "https://cdn.jsdelivr.net/gh/nicolaspanel/numjs@0.15.1/dist/numjs.min.js"
+    },
+    'COMPROMISE': {
+        'JS': "https://unpkg.com/compromise"
+    },
+    'PAPAPARSE': {
+        'JS': "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.1/papaparse.min.js"
+    },
+    'SHEPHERD': {
+        'CSS': "https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/css/shepherd.css",
+        'JS': "https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/js/shepherd.min.js"
+    },
+    'WINK_NLP': {
+        'JS': "./static/lib/wink-nlp/bundle-1.8.0.min.js"
+    },
+    'DS_SPLITER': {
+        'JS': "./static/lib/ds-spliter/ds-spliter-1.0.0.min.js"
+    },
+    'XML_FORMATTER': {
+        'JS': "https://cdn.jsdelivr.net/npm/xml-formatter@2.4.0/dist/browser/xml-formatter.js"
     },
 }}
