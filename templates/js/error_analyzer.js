e02a3aab0176f966f52b44a444ad2138d1b41ba8
Huan He <hehuan2112@gmail.com>
2022-07-28 11:18:03 -0500

    create files for ea

---
+++ templates/js/error_analyzer.js
@@ -0,0 +1,14 @@
+/**
+ * Error Analyzer
+ * 
+ * A toolkit for analyzing the corpus error.
+ * The function is based on IAA calculator's output:
+ * 
+ * When using annotator A as Gold Standard Corpora (GSC),
+ * The annotator B can be regarded as a system to be assessed.
+ * The IAA between GSC and B, especially the false positive (FP)
+ * and false negative (FN) can be analyzed as errors.
+ */
+var error_analyzer = {
+
+};
