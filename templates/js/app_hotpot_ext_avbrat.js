68e8b133d95125ccac0cc4794fb3903257cff0a8
Huan He <hehuan2112@gmail.com>
2022-11-22 14:17:04 -0600

    add window for annviewer

---
+++ templates/js/app_hotpot_ext_avbrat.js
@@ -0,0 +1,30 @@
+/**
+ * This is an extension for annviewer brat vis
+ */
+
+/////////////////////////////////////////////////////////////////
+// annviewer-bratvis related variables
+/////////////////////////////////////////////////////////////////
+Object.assign(app_hotpot.vpp_data, {
+
+});
+
+
+/////////////////////////////////////////////////////////////////
+// annviewer-bratvis related functions
+/////////////////////////////////////////////////////////////////
+Object.assign(app_hotpot.vpp_methods, {
+
+    show_avbrat: function() {
+        $('.annviewer-bratvis').show();
+    },
+
+    show_avbrat_help: function() {
+
+    },
+
+    close_avbrat: function() {
+        $('.annviewer-bratvis').hide();
+    },
+
+});
