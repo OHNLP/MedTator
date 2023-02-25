800d99d59e32dacd85a431a65ed5e6bf03fbd5fa
Huan He <hehuan2112@gmail.com>
2023-02-25 17:16:03 -0600

    update page logic

---
+++ templates/js/jarvis.js
@@ -0,0 +1,120 @@
+var jarvis = {
+    // for annotating test
+    sample_text: {},
+
+    // for log
+    changelog_latest: '',
+
+    init: function() {
+        // too bad ...
+        if (isIE) { 
+            this.ssmsg(_NOT_SUPPORT_MSG);
+            return 0; 
+        }
+
+        app_hotpot.init();
+
+        if (isCHROME) { 
+
+            if (isFSA_API_OK) {
+                jarvis.ssmsg('Initializated')
+                setTimeout('jarvis.ssclose();', 500);
+
+            } else {
+                // this can be many reasons
+                if (isHTTPS) {
+                    // 2022-03-24: Thanks to Adam Cross@UIC who reported this bug!
+                    jarvis.ssmsg(_DISABLED_API_MSG);
+
+                } else if (isLOCALFILE) {
+                    // 2022-03-24: Thanks to Adam Cross@UIC who reported this bug!
+                    jarvis.ssmsg(_DISABLED_API_MSG);
+
+                } else if (!isLOCALHOST) {
+                    // not HTTPs and not localhost
+                    // so can not use FSA API
+                    jarvis.ssmsg(_SEC_LMT_MSG);
+
+                } else {
+                    // ?
+                    jarvis.ssmsg(_DISABLED_API_MSG);
+                }
+            }
+        } else {
+
+            if (isFSA_API_OK) {
+                jarvis.ssmsg('Initializated')
+                setTimeout('jarvis.ssclose();', 500);
+                
+            } else {
+                jarvis.ssmsg(_LMT_SUPPORT_MSG);
+            }
+
+        }
+
+        $(window).resize(function() {
+            app_hotpot.resize();
+        });
+
+        // get some settings here
+
+        // the default sample dataset for demo
+        var sample_ds = this.get_url_paramter('ds');
+        if (sample_ds == '') {
+            sample_ds = 'MINIMAL_TASK';
+        }
+
+        // show sample dataset at beginning or not
+        var show_sample = this.get_url_paramter('ss');
+        if (show_sample == 'yes') {
+            app_hotpot.vpp.load_sample_ds(sample_ds);
+        }
+
+        // show the tour?
+        var show_tour = this.get_url_paramter('st');
+        if (show_tour == 'yes') {
+            setTimeout('app_hotpot.start_tour_annotation();', 550);
+        }
+    },
+
+    get_url_paramter: function(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    },
+
+    ssmsg: function(msg) {
+        $('#ss-msg').html(msg);
+    },
+
+    ssclose: function() {
+        $('#start-screen').hide();
+    },
+
+    save_json: function(obj, fn) {
+        var json_text = JSON.stringify(obj, null, 4);
+        var blob = new Blob([json_text], {type: "text/json;charset=utf-8"});
+        saveAs(blob, fn);
+    },
+
+    save_vpp_as: function(name) {
+        if (app_hotpot.vpp.$data.dtd == null) {
+            console.log('* no dtd yet');
+        }
+        var dtd_name = app_hotpot.vpp.$data.dtd.name;
+
+        if (typeof(name) == 'undefined') {
+            name = dtd_name;
+        }
+
+        // change to upper case for better looking
+        name = name.toLocaleUpperCase();
+
+        // save it!
+        this.save_json(
+            app_hotpot.vpp.$data,
+            'vpp_data_'+name+'.json'
+        );
+    }
+}
---
+++ templates/js/jarvis_ext_sample_text.js
@@ -0,0 +1,23 @@
+/**
+ * This is an extension for jarvis
+ */
+
+/////////////////////////////////////////////////////////////////
+// Sample Texts for annotation test
+/////////////////////////////////////////////////////////////////
+Object.assign(jarvis.sample_text, {
+    'covid_vax': `Positive COVID-19 test with symptoms; 
+
+The patient had MS & ache (UoL < 2.4 & T'x > 5.5, with "AoE" found) was immune compromised; 
+
+This is a spontaneous report from a contactable consumer (patient). This female consumer of unspecified age received the first single dose of BNT162B2 (PFIZER-BIONTECH COVID-19 VACCINE) on 04Jan2021, for COVID-19 immunization. She had MS and was immune compromised. Concomitant medications were not reported. On 07Jan2021 the patient woke up with headache, runny nose, groggy and muscle aches. She went to pharmacy and got a rapid COVID test that resulted positive. She was asking if the 1st dose could have caused her to be positive on the test. Events outcome was unknown.  
+
+Information on the batch/lot number has been requested.
+
+Sender's Comments: 
+
+Based on the information available, a possible contributory role of the suspect products cannot be excluded for the reported event of positive for corona virus infection for the lack of efficacy of the vaccine. However, based on the mechanism of action of the vaccine, it is unlikely the patient would have fully developed immunity for the vaccine to be effective, due to the number of days passed since the vaccine is given. Case will be reevaluated based on follow-up information.
+
+As she was in D.C. on Wed. to attend Mr. LASTNAME's wedding at 9 A.M. to 14 P.M. with Ms. LASTNAME from Wash., Dr. LASTNAME updated the No. C8's comments and re-scheduled a virtual visit on Mon. of next week (Jul. 19th). 
+    `
+});
